0ce9a8294890ad0164287ae1af3f79960a9d82a2
Blust96 <q.bohain@gmail.com>
2020-05-14 14:43:40 +0200

    [UPDATE] Config update

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
+import * as config from 'config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = 3000;
+
+  const serverConfig = config.get('server')
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || serverConfig.port;
+
+  if(process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
   await app.listen(port);
   logger.log(`App listening on port ${port}`)
+
 }
 bootstrap();
